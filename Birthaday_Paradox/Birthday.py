3502062e7c0648c6ab0e2062ab6148aa76968b45
Vade-20 <vedantsaxena2012@gmail.com>
2023-07-03 22:22:09 +0530

    changing func name

--- Birthaday_Paradox/Birthday.py
+++ Birthaday_Paradox/Birthday.py
@@ -2,7 +2,7 @@ from random import randint
 from tqdm import tqdm
 
 
-def generate_new_bday(birthday:int):
+def check_common_bdays(birthday:int):
     bday_list= []
     for _ in range(birthday):
         month = randint(1,12)
@@ -20,11 +20,11 @@ def generate_new_bday(birthday:int):
     else:
         return False
     
-def main_game(bday):
+def start(bday):
     c = 0
     print("Counting Duplicates")
     for _ in tqdm(range(100_000)):
-        if generate_new_bday(bday):
+        if check_common_bdays(bday):
             c+=1
     percentage = (c/100_000)*100
     
@@ -38,7 +38,7 @@ if __name__=='__main__':
         while not (str(bday)).isdigit() or bday>100:
             print('Please enter an integer less than 100')
             bday = int(input('Please enter number of birthday your want to find percentage:'))
-        ans = main_game(bday)
+        ans = start(bday)
         print(f'In a group of {bday} individuals, the probability of having a common birthday is {ans}.')
         choice = input('Would you like to try again? (Y/N) ')
 

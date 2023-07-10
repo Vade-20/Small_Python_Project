45ab271e6969279dd18c98e4eb0e192f8d3fe86b
Vade-20 <vedantsaxena2012@gmail.com>
2023-07-10 08:40:39 +0530

    Adding readme

---
+++ BouncingDVD/README.md
@@ -0,0 +1,16 @@
+# Bouncing DVD Animation
+
+This is a Python script that creates a simple DVD screensaver animation using the Tkinter library. The animation consists of a bouncing DVD logo that changes color each time it hits the edges of the canvas.
+
+## Requirements
+
+- Python 3.x
+- Tkinter library
+
+## Customization
+
+You can customize the animation by modifying the code. Here are some possible modifications:
+
+- **Colors**: The `colours` list contains the colors used for the DVD logo. You can add or remove colors to customize the animation.
+- **Animation Speed**: The `root.after(10, moves)` line determines the speed of the animation. The number `10` represents the delay in milliseconds between each animation frame. You can increase or decrease this value to speed up or slow down the animation.
+
--- BouncingDVD/bouncingdvd.py
+++ BouncingDVD/bouncingdvd.py
@@ -1,38 +1,39 @@
 from tkinter import *
-
-colours = ['red','green','blue','yellow']
+from random import randint
+colours = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 root = Tk()
 canvas_size_x = 1000
 canvas_size_y = 500
-x_corr=1
-y_corr=1
+x_corr=2
+y_corr=2
 choice = 0
 root.geometry(f'{canvas_size_x}x{canvas_size_y}')
+root.resizable(False,False)
 canvas = Canvas(root,bg='black',highlightthickness=0)
 canvas.pack(fill='both',expand=True)
-dvd = canvas.create_text(50,50,text='DVD',fill=colours[choice],font=('Times new roman',35),width=101)
+dvd = canvas.create_text(randint(50,950),randint(50,480),text='DVD',fill=colours[choice],font=('Times new roman',35),width=101)
 
 def moves():
     global x_corr,y_corr,dvd,choice
     x,y = canvas.coords(dvd)
     if int(x)>950:
-        choice = choice + 1 if choice <3 else 0
+        choice = choice + 1 if choice <6 else 0
         canvas.itemconfig(dvd, fill=colours[choice])
-        x_corr=-1
+        x_corr=-2
         
     elif int(x)<50:
-        choice = choice + 1 if choice <3 else 0
+        choice = choice + 1 if choice <6 else 0
         canvas.itemconfig(dvd, fill=colours[choice])
-        x_corr=1
+        x_corr=2
         
     if int(y)>480:
-        choice = choice + 1 if choice <3 else 0
+        choice = choice + 1 if choice <6 else 0
         canvas.itemconfig(dvd, fill=colours[choice])
-        y_corr=-1
+        y_corr=-2
     elif int(y)<20:
-        choice = choice + 1 if choice <3 else 0
+        choice = choice + 1 if choice <6 else 0
         canvas.itemconfig(dvd, fill=colours[choice])
-        y_corr=1
+        y_corr=2
         
     canvas.move(dvd,x_corr,y_corr)
     return root.after(10,moves)

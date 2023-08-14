517e4b66dc58b2c2062d560d0f5512f696cb280b
Vade-20 <vedantsaxena2012@gmail.com>
2023-08-14 22:40:34 +0530

    Excep_keyboard

--- Digital_clock/digital_clock.py
+++ Digital_clock/digital_clock.py
@@ -10,18 +10,21 @@ def main(stdscr):
   curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
   YELLOW_BLACK = curses.color_pair(1)
   while True:
-    current_time = localtime()
-    hours = current_time.tm_hour if len(str(current_time.tm_hour))==2 else '0'+str(current_time.tm_hour)
-    minutes = current_time.tm_min if len(str(current_time.tm_min))==2 else '0'+str(current_time.tm_min)
-    seconds = current_time.tm_sec if len(str(current_time.tm_sec))==2 else '0'+str(current_time.tm_sec)
-    t = f"{hours}:{minutes}:{seconds}"
-    a = 0 
-    stdscr.clear()
-    for i in str(t):
-      i = int(i) if i.isdigit() else i
-      cur_win = curses.newwin(0,0,15,a+15)
-      num = number_patterns.get(i)
-      cur_win.addstr(num,YELLOW_BLACK)
-      a +=10
-      cur_win.refresh()
-    sleep(1)
+    try:
+      current_time = localtime()
+      hours = current_time.tm_hour if len(str(current_time.tm_hour))==2 else '0'+str(current_time.tm_hour)
+      minutes = current_time.tm_min if len(str(current_time.tm_min))==2 else '0'+str(current_time.tm_min)
+      seconds = current_time.tm_sec if len(str(current_time.tm_sec))==2 else '0'+str(current_time.tm_sec)
+      t = f"{hours}:{minutes}:{seconds}"
+      a = 0 
+      stdscr.clear()
+      for i in str(t):
+        i = int(i) if i.isdigit() else i
+        cur_win = curses.newwin(0,0,15,a+15)
+        num = number_patterns.get(i)
+        cur_win.addstr(num,YELLOW_BLACK)
+        a +=10
+        cur_win.refresh()
+      sleep(1)
+    except KeyboardInterrupt:
+      break

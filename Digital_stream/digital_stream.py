32f9bed0450b430584bd6c4b7f597c1aaf2fa07f
Vade-20 <vedantsaxena2012@gmail.com>
2023-09-10 08:10:55 +0530

    FInal_commit

--- Digital_stream/digital_stream.py
+++ Digital_stream/digital_stream.py
@@ -1,32 +1,33 @@
-import time
+from time import sleep
 import random
 import os
 
-column = os.get_terminal_size().columns
-MIN_STREAM_SIZE = 6
-MAX_STREAM_SIZE = 14
-PAUSE = 1
-DENSITY = 10
+column = int(os.get_terminal_size().columns)
 
-density = [i for i in range(DENSITY)]
+MIN_STREAM_SIZE = 6         #Enter how long a single stream should be
+MAX_STREAM_SIZE = 14
+
+PAUSE = 0.1                 #Enter the pause each stream should have after printing it out
+DENSITY = 10                #Enter the density percentage ie the percantage of terminal you want the screen to appear in eg 10%,50%,70%
 stream = {}
-print('Digital Stream')
+print('\033[32mDigital Stream')
 print('Press Ctrl+C to quit')
+sleep(1)
 while True:
     try:
-        while len(stream)<DENSITY:
-            stream.setdefault(random.randint(0,column) , random.randint(MIN_STREAM_SIZE,MAX_STREAM_SIZE))
+        while len(stream) != (int((DENSITY*column)/100)):
+            stream[random.randint(0,column)] = random.randint(MIN_STREAM_SIZE,MAX_STREAM_SIZE)
         for i in range(0,column):
-            if i not in stream:
-                print(end=' ')
-            else: 
-                print(f'{random.randint(0,1)}')
+            if i in stream:
+                print(f'{random.randint(0,1)}',end='',flush=True)
                 stream[i] -= 1
                 if stream[i]<1:
                     stream.pop(i)
-                    
-        time.sleep(PAUSE)
+            else:
+                print(' ',end='',flush=True)
+        print('',flush=True)
+        sleep(PAUSE)
     except KeyboardInterrupt:
         break
-
-print('Thank you ')
+print('')
+print('Thank you \033[0m')

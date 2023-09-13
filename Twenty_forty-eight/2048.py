d93eaab70fa3370d3b61c7f11d4996cf131b449a
Vade-20 <vedantsaxena2012@gmail.com>
2023-09-13 22:52:50 +0530

    adding_moves

--- Twenty_forty-eight/2048.py
+++ Twenty_forty-eight/2048.py
@@ -1,8 +1,8 @@
 import random
 import curses
 from curses import wrapper
-
-
+from time import sleep
+from pprint import pprint
 NUM_OF_ROWS = 4
 NUM_OF_COLS = 4
 
@@ -14,9 +14,10 @@ def get_colours(a):
         curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
         curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        curses.init_pair(8, curses.COLOR_RED, curses.COLOR_WHITE)
-        curses.init_pair(9, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(10, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(8, curses.COLOR_RED,curses.COLOR_WHITE)
+        curses.init_pair(9, curses.COLOR_BLUE, curses.COLOR_WHITE)
+        curses.init_pair(10, curses.COLOR_GREEN, curses.COLOR_WHITE)
+        curses.init_pair(11, curses.COLOR_WHITE,curses.COLOR_RED)
 
         
         colors ={'2': curses.color_pair(7)
@@ -25,14 +26,16 @@ def get_colours(a):
                 ,'16':curses.color_pair(6)
                 ,'32':curses.color_pair(4)
                 ,'64':curses.color_pair(5)
-                ,'128':curses.color_pair(7)
-                ,'256':curses.color_pair(9)
-                ,'512':curses.color_pair(10)
-                ,'1024':curses.color_pair(11)
-                ,'2048':curses.color_pair(8)}
+                ,'128':curses.color_pair(1)
+                ,'256':curses.color_pair(8)
+                ,'512':curses.color_pair(9)
+                ,'1024':curses.color_pair(10)
+                ,'2048':curses.color_pair(11)}
         return colors.get(a)
 
 def get_board(stdsrc,board):
+    stdsrc.clear()
+    stdsrc.refresh()
     ans = ''
     for i in range(NUM_OF_ROWS):
         ans += '+-------'*NUM_OF_COLS+'+\n'
@@ -59,13 +62,91 @@ Press Enter to begin...    '''
     stdsrc.clear()
     stdsrc.refresh()
 
+def moves(board,move,stdsrc):
+    step = 1
+    for i in range(NUM_OF_ROWS):
+        for j in range(NUM_OF_COLS):
+            if board.get((i,j))!=' ':
+                num_u = 1
+                num_l = 0
+                if move =='w': 
+                    while True:
+                        if board.get((i-num_u,j))==' ':
+                            board[(i-num_u,j)] = board[(i-num_l,j)]
+                            board[(i-num_l,j)] = ' '
+                            num_u += 1
+                            num_l += 1
+                            stdsrc.refresh()
+                            continue
+                        elif board.get((i-num_u,j))==board[(i-num_l,j)]:
+                            board[(i-num_u,j)] = str(2*int( board[(i-num_u,j)]))
+                            board[(i-num_l,j)] = ' '
+                        break
+                if move =='s': 
+                    while True:
+                        if board.get((i+num_u,j))==' ':
+                            board[(i+num_u,j)] = board[(i+num_l,j)]
+                            board[(i+num_l,j)] = ' '
+                            num_u += 1
+                            num_l += 1
+                            stdsrc.refresh()
+                            continue
+                        elif board.get((i+num_u,j))==board[(i+num_l,j)]:
+                            board[(i+num_u,j)] = str(2*int( board[(i+num_u,j)]))
+                            board[(i+num_l,j)] = ' '
+                        break
+                if move =='d': 
+                    while True:
+                        if board.get((i,j+num_u))==' ':
+                            board[(i,j+num_u)] = board[(i,j+num_l)]
+                            board[(i,j+num_l)] = ' '
+                            num_u += 1
+                            num_l += 1
+                            stdsrc.refresh()
+                            continue
+                        elif board.get((i,j+num_u))==board.get((i,j+num_l)):
+                            board[(i,j+num_u)] = str(2*int( board[(i,j+num_u)]))
+                            board[(i,j+num_l)] = ' '
+                        break
+                if move =='a': 
+                    while True:
+                        if board.get((i,j-num_u))==' ':
+                            board[(i,j-num_u)] = board[(i,j-num_l)]
+                            board[(i,j-num_l)] = ' '
+                            num_u += 1
+                            num_l += 1
+                            stdsrc.refresh()
+                            continue
+                        elif board.get((i,j-num_u))==board.get((i,j-num_l)):
+                            board[(i,j-num_u)] = str(2*int( board[(i,j-num_u)]))
+                            board[(i,j-num_l)] = ' '
+                        break
+
+                            
 @wrapper
 def main(stdsrc):
     board = {(i,j):' ' for i in range(NUM_OF_ROWS) for j in range(NUM_OF_COLS)}
+    pprint(board)
+    board[random.randint(0,NUM_OF_ROWS-1),random.randint(0,NUM_OF_COLS-1)] = '2'
+    board[random.randint(0,NUM_OF_ROWS-1),random.randint(0,NUM_OF_COLS-1)] = '2'
+    curses.echo()
     stdsrc.clear()
     stdsrc.refresh()
     rules(stdsrc)
     while True:
         get_board(stdsrc,board)
-        break
+        stdsrc.addstr('Enter move: (WASD or Q to quit) : ',get_colours('128'))
+        ans = stdsrc.getstr().decode(encoding='utf-8')
+        if ans.lower() not in ['w','a','s','d','q']:
+            continue
+        elif ans.lower()=='q':
+            quit()
+        moves(board,ans,stdsrc)
+        while True:
+            row_ = random.randint(0,NUM_OF_ROWS-1)
+            col_ = random.randint(0,NUM_OF_COLS-1)
+            if board.get((row_,col_))==' ':
+                board[(row_,col_)] = '2'
+                break
+        
     stdsrc.getch()

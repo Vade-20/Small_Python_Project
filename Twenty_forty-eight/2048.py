ee552980809564907ee1792f3ee8454f19d32fc3
Vade-20 <vedantsaxena2012@gmail.com>
2023-09-14 13:54:43 +0530

    adding_winning_condition

--- Twenty_forty-eight/2048.py
+++ Twenty_forty-eight/2048.py
@@ -1,23 +1,9 @@
 import random
 import curses
 from curses import wrapper
-from time import sleep
-from pprint import pprint
+
 NUM_OF_ROWS = 4
 NUM_OF_COLS = 4
-
-def get_colours(a):
-    color_codes = {
-    '1': "\033[31m",           #red
-    '2': "\033[38;5;208m",     #orange
-    '3': "\033[33m",           #yellow 
-    '4': "\033[32m",           #green
-    '5': "\033[34m",           #blue
-    '6': "\033[38;5;54m",      #indigo
-    '7': "\033[38;5;128m",     #violet
-    "reset": "\033[0m"
-}
-    return color_codes.get(a)
 
 def get_board(stdsrc,board):
     stdsrc.clear()
@@ -34,6 +20,7 @@ def get_board(stdsrc,board):
     ans += '+-------'*NUM_OF_COLS+'+\n'
     stdsrc.addstr(ans)
     stdsrc.refresh()            
+
 
 def up(board):
     for i in range(NUM_OF_ROWS):
@@ -53,6 +40,7 @@ def up(board):
                         board[(i-num_l,j)] = ' '
                     break   
 
+
 def down(board):
     for i in range(NUM_OF_ROWS-1,-1,-1):
         for j in range(NUM_OF_COLS-1,-1,-1):
@@ -71,6 +59,7 @@ def down(board):
                         board[(i+num_l,j)] = ' '
                     break
 
+
 def left(board):
     for j in range(0,NUM_OF_COLS):
         for i in range(0,NUM_OF_ROWS):
@@ -88,6 +77,7 @@ def left(board):
                         board[(i,j-num_u)] = str(2*int( board[(i,j-num_u)]))
                         board[(i,j-num_l)] = ' '
                     break
+    
                 
 def right(board):
     for j in range(NUM_OF_COLS-1,-1,-1):
@@ -108,20 +98,36 @@ def right(board):
                     break
         
     
+    
 def rules(stdsrc):
-    rules = '''
-Slide all the tiles on the board in one of four directions. Tiles with
-like numbers will combine into larger-numbered tiles. A new 2 tile is
-added to the board on each move. You win if you can create a 2048 tile.
-You lose if the board fills up tiles and there is no more moves left.
-Press Enter to begin...    '''
-    stdsrc.addstr(5,5,rules,get_colours('128'))
+    rules = f'''
+RULES OF 2048 ~
+Certainly, here are the rules of the game 2048 presented in bullet points:
+
+- **Objective**: The goal of 2048 is to reach the tile with the number 2048 by sliding and combining numbered tiles on a 4x4 grid.
+
+- **Starting Grid**: The game starts with a {NUM_OF_ROWS}x{NUM_OF_COLS} grid that contains two tiles, each with the number 2.
+
+- **Tile Movement**: You can move tiles in four directions: up, down, left, and right using WASD keys.
+
+- **Tile Combining**: When two tiles with the same number collide while moving in a direction, they merge into one tile with a number that is the sum of the two original tiles. For example, two tiles with '2' will merge into one tile with '4'.
+
+- **Random Tile Generation**: After each move, a new tile with a value of 2 or 4 appears on the grid at a random empty spot.
+
+- **Winning**: You win the game when you create a tile with the number 2048 by merging tiles. 
+
+- **Losing**: The game ends when the grid is full.
+
+- **Scoring**: Your score is calculated based on the value of the tiles you combine. Each merge adds the value of the merged tiles to your score. For example, merging two '4' tiles would give you 8 points.
+ 
+PRESS ENTER YO CONTINUE'''
+
+    stdsrc.addstr(5,5,rules,curses.color_pair(1))
     stdsrc.getch()
     stdsrc.clear()
     stdsrc.refresh()
 
 def moves(board,move,stdsrc):
-    step = 1
     direction = {'w':up,'s':down,'a':left,'d':right}
     direction.get(move)(board)
 
@@ -131,24 +137,47 @@ def main(stdsrc):
     board = {(i,j):' ' for i in range(NUM_OF_ROWS) for j in range(NUM_OF_COLS)}
     board[random.randint(0,NUM_OF_ROWS-1),random.randint(0,NUM_OF_COLS-1)] = '2'
     board[random.randint(0,NUM_OF_ROWS-1),random.randint(0,NUM_OF_COLS-1)] = '2'
+    
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.echo()
     stdsrc.clear()
     stdsrc.refresh()
+    
     rules(stdsrc)
     while True:
+        losing = [int(board.get(i)) for i in board if board[i]!=' ']
+        if 2048 in losing:
+            stdsrc.clear()
+            stdsrc.addstr(10,10,"YOU WIN!!!!!!!!!!!!!!!!",curses.color_pair(1))
+            stdsrc.addstr(12,10,f"Your score is {sum(losing)}",curses.color_pair(1))
+            stdsrc.addstr(14,10,"Press enter to exit",curses.color_pair(1))
+            stdsrc.getch()
+            quit()
+            
+        if len(losing)>=NUM_OF_COLS*NUM_OF_ROWS:
+            stdsrc.clear()
+            stdsrc.addstr(10,10,"GAME OVER YOU LOSE!!!!!!!!!!!!!!!!",curses.color_pair(1))
+            stdsrc.addstr(12,10,f"Your score is {sum(losing)}",curses.color_pair(1))
+            stdsrc.addstr(14,10,"Press enter to exit",curses.color_pair(1))
+            stdsrc.getch()
+            quit()
+            
         get_board(stdsrc,board)
-        stdsrc.addstr('Enter move: (WASD or Q to quit) : ',get_colours('128'))
+        stdsrc.addstr(f'Your score:{sum(losing)}\nEnter move: (WASD or Q to quit) : ',curses.color_pair(1))
         ans = stdsrc.getstr().decode(encoding='utf-8')
         if ans.lower() not in ['w','a','s','d','q']:
             continue
         elif ans.lower()=='q':
             quit()
         moves(board,ans,stdsrc)
+        
         while True:
             row_ = random.randint(0,NUM_OF_ROWS-1)
             col_ = random.randint(0,NUM_OF_COLS-1)
             if board.get((row_,col_))==' ':
-                board[(row_,col_)] = '2'
+                if random.randint(1,100)<81:
+                    board[(row_,col_)] = '2'
+                else:
+                    board[(row_,col_)] = '4'
                 break
         
-    stdsrc.getch()

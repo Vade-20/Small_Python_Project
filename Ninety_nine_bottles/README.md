0915b90e707b75ec0f26c0b47394870ae1c43b55
Vade-20 <vedantsaxena2012@gmail.com>
2023-10-10 20:17:38 +0530

    exception_keyboard

---
+++ Ninety_nine_bottles/README.md
@@ -0,0 +1,13 @@
+# Ninety Nine Bottles
+
+## Introduction
+This Python script uses the curses library to create an animated countdown of milk bottles on the wall. It simulates the classic "99 Bottles of Milk on the Wall" song, where bottles gradually disappear as the countdown progresses. The animation is displayed in the terminal.
+
+## Requirements
+Before running the script, you'll need to have Python installed on your system. Additionally, make sure you have the `curses` library, which is commonly available in Python's standard library.
+
+## Customization
+You can customize the animation by adjusting the following variables:
+- `PAUSE`: Controls the pause time between each step of the animation.
+- The appearance of the milk bottle in the `BOTTLE` variable.
+- Text messages and their timing in the animation loop.
--- Ninety_nine_bottles/ninety_nine_bottles.py
+++ Ninety_nine_bottles/ninety_nine_bottles.py
@@ -1,10 +1,12 @@
 import curses
 from curses import wrapper
-import shutil
+from shutil import get_terminal_size
 from time import sleep
-x_terminal,y_terminal = shutil.get_terminal_size()
-PAUSE = 0.9
-bottle = ''' ||
+
+x_terminal,y_terminal = get_terminal_size()
+PAUSE = 0.5
+
+BOTTLE = ''' ||
  )(
 |__|
 |  |
@@ -13,33 +15,51 @@ bottle = ''' ||
 @wrapper
 def main(stdsrc):
     TOTAL_BOTTLES = 99
-    stdsrc.clear()
-    stdsrc.refresh()
-    while TOTAL_BOTTLES>0:
-        total_bottles = TOTAL_BOTTLES
-        for i in range(0,25,6):
-            for j in range(0,120,5):
-                    cur_new = curses.newwin(5,5,i,j)
-                    cur_new.addstr(bottle)
-                    cur_new.refresh()
-                    total_bottles -= 1
-                    if total_bottles < 0:
-                        break
-            if total_bottles<0:
-                break
-            stdsrc.addstr(i+5,0,'-'*(x_terminal-1)+'\n')
-        stdsrc.addstr(24,21,f'{TOTAL_BOTTLES} bottles of milk on the wall,')
-        sleep(PAUSE)
-        stdsrc.refresh()
-        stdsrc.addstr(25,21,f"{TOTAL_BOTTLES} bottles of milk")
-        sleep(PAUSE)
-        stdsrc.refresh()
-        stdsrc.addstr(26,21,'Take one down, pass it around,')
-        sleep(PAUSE)
-        stdsrc.refresh()
-        stdsrc.addstr(27,21,f'{TOTAL_BOTTLES-1} bottles of milk on the wall!')
-        stdsrc.refresh()
-        sleep(PAUSE)
-        TOTAL_BOTTLES -= 1
-        stdsrc.clear()
+    curses.curs_set(0)
+    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_YELLOW,curses.COLOR_BLACK)
+    GREEN = curses.color_pair(1)
+    YELLOW = curses.color_pair(2)
+    adjust_y = 0
+    try:
+        while TOTAL_BOTTLES>0:
+            stdsrc.clear()
+            stdsrc.refresh()
+            total_bottles = TOTAL_BOTTLES
+            
+            for i in range(0,25,6):
+                for j in range(0,120,5):
+                        cur_new = curses.newwin(5,5,i,j)
+                        cur_new.addstr(BOTTLE,YELLOW)
+                        cur_new.refresh()
+                        total_bottles -= 1
+                        if total_bottles < 0:
+                            break
+                if total_bottles<0:
+                    break
+                stdsrc.addstr(i+5,0,'-'*(x_terminal-1)+'\n')
+                stdsrc.refresh()
+            if TOTAL_BOTTLES<96:
+                adjust_y = 20    
+            stdsrc.addstr(24,21-adjust_y,f'{TOTAL_BOTTLES} bottles of milk on the wall,',GREEN)
+            stdsrc.refresh()
+            sleep(PAUSE)
+            
+            stdsrc.addstr(25,21-adjust_y,f"{TOTAL_BOTTLES} bottles of milk",GREEN)
+            stdsrc.refresh()
+            sleep(PAUSE)
+            
+            stdsrc.addstr(26,21-adjust_y,'Take one down, pass it around',GREEN)
+            stdsrc.refresh()
+            sleep(PAUSE)
+            
+            stdsrc.addstr(27,21-adjust_y,f'{TOTAL_BOTTLES-1} bottles of milk on the wall!',GREEN)
+            stdsrc.refresh()
+            sleep(PAUSE)
+            
+            TOTAL_BOTTLES -= 1
+            
+    except KeyboardInterrupt:
+        quit()
+            
         

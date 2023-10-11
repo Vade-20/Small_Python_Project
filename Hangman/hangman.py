d368b2036952f8c2cf170d42198602cb2fab4fcb
Vade-20 <vedantsaxena2012@gmail.com>
2023-10-11 21:18:19 +0530

    added_all_files

---
+++ Hangman/hangman.py
@@ -0,0 +1,99 @@
+from hangman_visual import visuals
+import json
+import random
+from PyDictionary import PyDictionary
+import curses
+from curses import wrapper
+
+dictionary = PyDictionary()
+
+def get_a_word():
+    try:
+        with open(r'Hangman\words.json', 'r',encoding='utf-8') as f:
+            data = list(json.load(f))
+    except FileNotFoundError:
+        print('Please change directory to where the words.json is located.')
+        quit()
+        
+    random_word = random.choice(data).lower()
+    meaning = dictionary.meaning(random_word, disable_errors=True)
+    count = 0
+    while meaning is None:
+        if count == 5:
+            meaning = {"Internet connection unavailable":["Please establish an internet connection to retrieve the meaning of the word."]}
+            break
+        meaning = dictionary.meaning(random_word, disable_errors=True)
+        count += 1
+    meaning_type = list(meaning.keys())[0]
+    meaning = ','.join(meaning[meaning_type])
+    return (random_word,meaning_type,meaning)
+
+
+@wrapper
+def main(stdsrc):
+    stdsrc.clear()
+    curses.echo()
+    curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
+    YELLOW = curses.color_pair(1)
+    RED = curses.color_pair(2)
+    stdsrc.addstr(3,2,"HANGMAN THE GAME",RED)
+    stdsrc.refresh()
+    
+    visuals_window = curses.newwin(10,30,5,5)
+    visuals_constant = 7
+    visuals_window.addstr(2,5,f'{visuals[visuals_constant]}',YELLOW)
+    visuals_window.border()
+    visuals_window.refresh()
+    
+    word_already_used = ['Missed letters-']
+    word_used_win = curses.newwin(10,21,5,40)
+    word_used_win.addstr(1,2,f'{word_already_used[0]}',RED)
+    word_used_win.border()
+    word_used_win.refresh()
+    
+    stdsrc.addstr(19,5,f'LOADING.....',RED)
+    stdsrc.refresh()
+    word = get_a_word()
+    
+    mising_words = '_ '*len(word[0])
+    mising_words_win = curses.newwin(1,30,17,5)
+    mising_words_win.addstr(mising_words,RED)
+    mising_words_win.refresh()
+    
+    stdsrc.addstr(19,5,f'Definition : {word[1]} : {word[2]}',RED)
+    stdsrc.refresh()
+    word = ''.join([f'{i} ' for i in word[0]])
+    print(word)
+    
+    while visuals_constant>0:
+        stdsrc.addstr(21,5,f'Guess a letter\n     >',RED)
+        ans = chr(stdsrc.getch())
+
+        if not ans.isalpha():
+            continue
+        
+        if ans in word:
+            mising_words = list(mising_words)
+            for i in range(0,len(word)-1):
+                if word[i] == ans:
+                    mising_words[i] = ans
+            mising_words = ''.join(mising_words)
+            mising_words_win.clear()
+            mising_words_win.addstr(mising_words)
+            mising_words_win.refresh()
+        else:
+            visuals_constant -= 1
+            visuals_window.clear()
+            visuals_window.addstr(2,5,f'{visuals[visuals_constant]}',YELLOW)
+            visuals_window.border()
+            visuals_window.refresh()
+            
+            word_already_used.append(ans)
+            word_used_win.clear()
+            for i,j in enumerate(word_already_used,start=1):
+                word_used_win.addstr(i,2,j)
+            word_used_win.border()
+            word_used_win.refresh()
+            
+

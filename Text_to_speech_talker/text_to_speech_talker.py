a31973c3f27752e3c6245ddfe679d2435fb74be5
Vade-20 <vedantsaxena2012@gmail.com>
2023-10-24 09:40:07 +0530

    Added_settings

--- Text_to_speech_talker/text_to_speech_talker.py
+++ Text_to_speech_talker/text_to_speech_talker.py
@@ -28,10 +28,10 @@ vcmd = (root.register(validate_input), '%P')
 voices = engine.getProperty('voices')
 rate = engine.getProperty('rate')
 volume = engine.getProperty('volume')
-
 num_of_voices = []
 for voice in voices:
      num_of_voices.append((voice.name,voice.id))
+
 
 def speak():
     a = t1.get(1.0,END)
@@ -39,15 +39,15 @@ def speak():
     engine.runAndWait()
 
 def back():
+    global rate,volume
     for i in num_of_voices:
-        if i[0] == option_var:
+        if i[0] == option_var.get():
             engine.setProperty('voice', i[1])
     rate = int(spin_3.get())
-    volume = int(spin_4.get())/10
     engine.setProperty('rate',rate)
+    volume = int(spin_4.get())/100
     engine.setProperty('volume',volume)
     main()
-    
 
 def clear_screen() :
     widget = root.winfo_children()
@@ -58,10 +58,17 @@ def clear_screen() :
         item.destroy()
 
 def setting():
-    global option_2,spin_3,spin_4,option_var
+    global option_2,spin_3,spin_4,option_var,rate,volume
     
     root.geometry('670x220')
     clear_screen()
+    option_var = StringVar()
+    option_var.set(num_of_voices[0][0])
+    spin_3 = IntVar()
+    spin_3.set(rate)
+    spin_4 = IntVar()
+    spin_4.set(volume)
+
     
     l1 = Label(root, text='Settings', fg=FONT_COLOR, bg=BACKGROUND_COLOR , padx=10,font=('Times', '30'), bd=3, relief='sunken')
     l1.grid(row=0, column=0, columnspan=2,sticky=W+E)
@@ -75,19 +82,19 @@ def setting():
     l4 = Label(root, text='Volume --->', fg=FONT_COLOR, bg=BACKGROUND_COLOR , padx=50, font=('Times New Roman', 18), bd=3, relief='sunken')
     l4.grid(row=4, column=0, sticky=W+E)
 
-    option_var = StringVar()
+
     option_2 = OptionMenu(root, option_var, *[i[0] for i in num_of_voices])
     option_2.config(fg=FONT_COLOR, bg=BACKGROUND_COLOR , font=('Times', '15'), width=40, relief='sunken')
     option_2.grid(row=2, column=1)
-    option_var.set(num_of_voices[0][0])
 
-    spin_3 = Spinbox(root, from_= 0, to = 4000,validate="key", validatecommand=vcmd,justify='center',font=('Times New Roman',15),fg=FONT_COLOR, bg=BACKGROUND_COLOR ) 
+    spin_3 = Spinbox(root, from_= 0, to = 4000,increment=10,validate="key",textvariable=spin_3, validatecommand=vcmd,justify='center',font=('Times New Roman',15),fg=FONT_COLOR, bg=BACKGROUND_COLOR ) 
     spin_3.grid(row=3,column=1,sticky=W+E)
-    spin_3.insert(0,rate)
+
     
-    spin_4 = Spinbox(root, from_= 0, to = 100,validate="key", validatecommand=vcmd,justify='center',font=('Times New Roman',15),fg=FONT_COLOR, bg=BACKGROUND_COLOR ) 
+    spin_4 = Spinbox(root, from_= 0, to = 100,increment=10,validate="key",textvariable=spin_4, validatecommand=vcmd,justify='center',font=('Times New Roman',15),fg=FONT_COLOR, bg=BACKGROUND_COLOR ) 
     spin_4.grid(row=4,column=1,sticky=W+E)
-    spin_4.insert(0,round(volume*10))
+    spin_4.delete(0,'end')
+    spin_4.insert(0,int(volume*100))
     
     b1 = Button(root,text = 'BACK',fg=FONT_COLOR,bg = BACKGROUND_COLOR,font= ('Times New Roman', '15'),command=back,width=15,relief='raised',justify='center')
     b1.grid(row=5,column=0,columnspan=2,sticky=W+E)
@@ -104,7 +111,7 @@ def main():
     l__ = Label(root, text='                  ', fg=FONT_COLOR, bg=BACKGROUND_COLOR , padx=250, bd=3, relief='sunken')
     l__.grid(row=5, column=0, columnspan=6,sticky=W+E)
 
-    t1 = scrolledtext.ScrolledText(root, wrap = WORD, width = 40, height = 10, font = ("Times New Roman",15)) 
+    t1 = scrolledtext.ScrolledText(root, wrap = WORD, width = 40, height = 10, font = ("Times New Roman",15),fg=FONT_COLOR) 
     t1.insert(1.0,'Text here')
     t1.grid(row=2,column=0,columnspan=6,rowspan=3,sticky=W+E)
     

38ebaacc6e3b4c62828262f1ea40a8701b2fd0ad
Vade-20 <vedantsaxena2012@gmail.com>
2023-10-28 20:23:44 +0530

    instances_left

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -15,6 +15,22 @@ def validate_input(value):
     else:
         return False
 
+def instances_left():
+    num_of_instances = {}
+    for i in entry_list:
+        for j in i:
+            value = j.get()
+            if value != '':
+                num_of_instances.setdefault(value,9)
+                num_of_instances[value] -= 1
+            
+    str_num_of_instances = ''
+    for i in range(1,10):
+        str_num_of_instances += f'{i}:{num_of_instances[str(i)]}    '
+        
+    instance = Label(root, text=f'{str_num_of_instances}', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'), bd=3, relief='solid',justify='center')
+    instance.grid(row=15, column=0, columnspan=14,sticky=W+E)     
+    
 def new_game():
     for i in entry_list:
         for j in i:
@@ -23,7 +39,7 @@ def new_game():
             
     with open(r'Mini_python_project\Sudoku\sudokupuzzles.txt','r') as f:
         puzzles = f.readlines()
-    b1.destroy()
+
     puzzle = choice(puzzles).strip()
     use_less = 0
     for i in range(9):
@@ -33,17 +49,22 @@ def new_game():
                 entry_list[i][j].insert(0,value)
                 entry_list[i][j].config(state='readonly')
             else:
-                entry_list[i][j].insert(0,'')          
+                entry_list[i][j].insert(0,'')
+                
             use_less+=1
             
+    instances_left()
+                  
     l1 = Label(root, text='Sudoku', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
     l1.grid(row=0, column=0, columnspan=14,sticky=W+E)
+   
    
 def animation_of_button(event):
     if 'Enter' in str(event):
         b1.config(fg='black',relief='solid')   
     else:
         b1.config(fg=FONT_COLOR,relief='raised')
+
 
 def show_row_column(event,box):
     for i in entry_list:
@@ -67,10 +88,11 @@ def show_row_column(event,box):
                     j.config(readonlybackground='light grey')
                 j.config(bg='light grey')
                  
-   
-                   
+                     
 def winining_condition(event):
     global b1
+    
+    instances_left()
     box = []
     for entry in entry_list:
         box__= []
@@ -115,10 +137,7 @@ def winining_condition(event):
     l1.destroy()   
     l = Label(root, text='Congratulation!!!! You Won', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '30'), bd=3, relief='solid',justify='center')
     l.grid(row=0, column=0, columnspan=14,sticky=W+E)     
-    b1 = Button(root,text = 'NEW GAME',fg=FONT_COLOR,bg = BACKGROUND_COLOR,font= ('Times New Roman', '15'),command=new_game,width=15,relief='raised',justify='center')
-    b1.grid(row=15,column=0,columnspan=14,sticky=W+E)     
-    b1.bind('<Enter>',animation_of_button)    
-    b1.bind('<Leave>',animation_of_button)    
+
         
 vcmd = (root.register(validate_input), '%P')
 entry_list = []
@@ -127,6 +146,11 @@ l1 = Label(root, text='Sudoku', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times
 l1.grid(row=0, column=0, columnspan=14,sticky=W+E)
 l2 = Label(root, text='      ', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '2'), bd=3, relief='sunken',justify='center')
 l2.grid(row=1, column=0, columnspan=14,sticky=W+E)
+b1 = Button(root,text = 'NEW GAME',fg=FONT_COLOR,bg = BACKGROUND_COLOR,font= ('Times New Roman', '15'),command=new_game,width=15,relief='raised',justify='center')
+b1.grid(row=16,column=0,columnspan=14,sticky=W+E)     
+b1.bind('<Enter>',animation_of_button)    
+b1.bind('<Leave>',animation_of_button)   
+ 
 for i in range(2,14):
     if i in [5,9,13]:
         l_ = Label(root, text='-'*110, fg=FONT_COLOR ,bg=BACKGROUND_COLOR ,font=('Times', '10'), bd=3, relief='flat')
@@ -144,32 +168,33 @@ for i in range(2,14):
                 dummy_list.append(e1)
         entry_list.append(dummy_list)
         
-pp = [[4,3,5,2,6,9,7,8,1],[6,8,2,5,7,1,4,9,3],[1,9,7,8,3,4,5,6,2],
-    [8,2,6,1,9,5,3,4,7],[3,7,4,6,8,2,9,1,5],[9,5,1,7,4,3,6,2,8],
-    [5,1,9,3,2,6,8,7,4],[2,4,8,9,5,7,1,3,6],[7,6,3,4,1,8,2,5,'']]
 
-for i in range(9):
-    for j in range(9):
-        entry_list[i][j].insert(0,pp[i][j])
 
-'''
 with open(r'Mini_python_project\Sudoku\sudokupuzzles.txt','r') as f:
     puzzles = f.readlines()
 
-
 puzzle = choice(puzzles).strip()
-
+num_of_instances = {}
 use_less = 0
 for i in range(9):
     for j in range(9):
+        
         value = puzzle[use_less]
         if value != '.':
+            num_of_instances.setdefault(value,9)
+            num_of_instances[value] -= 1
             entry_list[i][j].insert(0,value)
             entry_list[i][j].config(state='readonly')
         else:
             entry_list[i][j].insert(0,'')
             
         use_less+=1
-'''        
+
+str_num_of_instances = ''
+for i in range(1,10):
+    str_num_of_instances += f'{i}:{num_of_instances[str(i)]}    '
+instance = Label(root, text=f'{str_num_of_instances}', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'), bd=3, relief='solid',justify='center')
+instance.grid(row=15, column=0, columnspan=14,sticky=W+E)     
+
 root.bind('<Key>',winining_condition)
 mainloop()

662c4d0a2a33acab8764a364c9338e1feb185f12
Vade-20 <vedantsaxena2012@gmail.com>
2023-11-30 17:01:43 +0530

    Optimising_minimax

--- Tic_Tac_Toe/tic_tac_toe.py
+++ Tic_Tac_Toe/tic_tac_toe.py
@@ -9,26 +9,49 @@ BACKGROUND_COLOR = 'light blue'
 FONT_COLOR = 'crimson'
 READONLY_COLOR_BG = 'light blue'
 READONLY_COLOR_FG = 'red'
-max_player = 'X' 
-num_of_turn = 0
-
 root.config(bg=BACKGROUND_COLOR)
 root.resizable(False,False)
 
+
 def new_game(event=None):
     # State a new game of tic tac toe
-    global turn
+    global turn,l3,chance_list
     l1 = Label(root, text='TIC TAC TOE', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
     l1.grid(row=0, column=0, columnspan=15,sticky=W+E)
     turn = False
     for i in boxes:
         for j in i:
             j.config(state='normal')
-            j.delete(0,END)
+            j.delete(0,END)            
+    l3.destroy()
+    chance_list = [players[mode.get()][0],players[mode.get()][1]] 
+    chance.set('First Turn')
+    l3 = OptionMenu(root,chance,*chance_list,command=who_goes_first)
+    l3.grid(row=1,column=6,columnspan=4) 
+    l3.config(fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'), bd=3, relief='raised',justify='center',width=20)
+
+
+def who_goes_first(event=None):
+    # Select if the player goes first or the Computer
+    # If no one is selected than the player goes first
+    
+    global l3
+    if mode.get() == 'Computer Hard':
+        position = random.choice([0,2,6,8])
+        box = boxes[position//3][position%3]
+        box.insert(0,"O")
+        box.config(state='disabled')
+    elif mode.get() == 'Computer Easy':
+        box = boxes[random.randint(0,2)][random.randint(0,2)]
+        box.insert(0,'O')
+        box.config(state='disabled')
+    
+    l3.destroy()
     text = players[mode.get()][turn]
     l3 = Label(root, text=text, fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'),justify='center')
     l3.grid(row=1, column=6,columnspan=4)
-    
+        
+        
 def animation_of_button(event):
     #When the mouse hover over the button the button color and font changes
     if 'Enter' in str(event):
@@ -39,11 +62,7 @@ def animation_of_button(event):
 
 def validate_input_entry(value):
     # check if the value is valid for the game
-    if turn is False and value == 'X':
-        return True
-    elif turn is True and value == 'O':
-        return True
-    elif value == '':
+    if value in ['X','O','']:
         return True
     else:       
         return False
@@ -72,7 +91,8 @@ def wining_condition(board):
 
 
 def minimax(state, num_empty_squares, turn):
-
+    # Here Computer is the maximiser while player is the minimiser
+    
     player = 'X' if turn is True else 'O' 
     if player == 'O':
         best = [-1, -infinity]
@@ -105,46 +125,34 @@ def minimax(state, num_empty_squares, turn):
                 best = score  # min value
 
     return best
-
-
-def remove_empty_boxes():
-    for i in boxes:
-        for j in i:
-            if j.get() == '' and j['state']=='disabled':
-                j.config(state='normal')
+       
                            
 def adding_turn(event,box):
+    # Implemend clicking the gameplay instead of typing
     if turn is False:
         game_play('X',box)
     else:
         game_play('O',box)
         
+        
 def game_play(value,box):
-    global l3,turn,num_of_turn
-    
-    if value not in ['X','O']:
-        value_1 = value.char.upper()
-    else:
-        value_1 = value 
-    
-    box.insert(0,value_1)
+    global l3,turn
+
+    box.insert(0,value)
     box.config(state='disabled')
-    num_of_turn += 1
     
-    if not validate_input_entry(value_1):
-        return None
     # Changes the label to show which player/computer turn it is
     l3.destroy()
     text = players[mode.get()][turn]
     l3 = Label(root, text=text, fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'),justify='center')
     l3.grid(row=1, column=6,columnspan=4)
     
+    ########## COMPUTER TURN #############
+    
+    num_of_empty_spaces_left = len(['' for i in boxes for j in i if j.get()==''])
     if mode.get() =='Computer Easy':
         turn = not turn
-        while True:
-            num_of_empty_spaces_left = len(['' for i in boxes for j in i if j.get()==''])
-            if num_of_empty_spaces_left==0:
-                break           #No empty spaces is left,it is a tie
+        while num_of_empty_spaces_left != 0:
             rand = boxes[random.randint(0,2)][random.randint(0,2)]
             if rand.get() == '':
                 rand.insert(0,"O")
@@ -152,14 +160,16 @@ def game_play(value,box):
                 break
     elif mode.get() =='Computer Hard':
         turn = not turn
-        num_empty_squares = len(['' for i in boxes for j in i if j.get() == ''])
-        ans = minimax([j.get() for i in boxes for j in i],num_empty_squares,False)
+        ans = minimax([j.get() for i in boxes for j in i],num_of_empty_spaces_left,False)
         box = boxes[ans[0]//3][ans[0]%3]
         box.insert(0,"O")
         box.config(state='disabled')
-
+        
+    turn = not turn    
+    check_if_win()  
     
-    turn = not turn      
+    
+def check_if_win():
     win = wining_condition([j.get() for i in boxes for j in i])
     if win == 'Tie':
         for i in boxes:
@@ -167,43 +177,49 @@ def game_play(value,box):
                 j.config(state='disabled')
         l1 = Label(root, text=f"It's a Tie ", fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
         l1.grid(row=0, column=0, columnspan=15,sticky=W+E)
-        
     elif win is not False:
         for i in  boxes:
             for j in i:
                 j.config(state='disabled')
         num = 0 if win == 'X' else 1
-        l1 = Label(root, text=f'Congratulation {players[mode.get()][num]} won the game', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
+        l1 = Label(root, text=f'      {players[mode.get()][num]} won the game', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
         l1.grid(row=0, column=0, columnspan=15,sticky=W+E)
 
 
 vcmd = (root.register(validate_input_entry), '%P')
 boxes = []
-players = {'PvP':('Player 1', 'Player 2'),"Computer Easy":('Player', 'Computer'),"Computer Hard":('Player', 'Computer'),"Computer Impossible":('Player', 'Computer')}
+players = {'PvP':('Player 1', 'Player 2'),"Computer Easy":('Player', 'Computer'),"Computer Hard":('Player', 'Computer')}
 turn = False
+max_player = 'X'  
 
+
+################################### ALL THE WIDGET USED IN THE GAME ############################################################### 
 l1 = Label(root, text='TIC TAC TOE', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '40'), bd=3, relief='solid',justify='center')
 l1.grid(row=0, column=0, columnspan=15,sticky=W+E)
 
 l2 = Label(root, text='Game Mode---->', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '12'), bd=3, relief='solid',justify='center')
 l2.grid(row=1, column=0,columnspan=2)
 mode = StringVar()
-options_list = ["PvP", "Computer Easy", "Computer Hard","Computer Impossible"] 
+options_list = ["PvP", "Computer Easy", "Computer Hard"] 
 mode.set('Computer Hard')
 o1 = OptionMenu(root,mode,*options_list,command=new_game)
 o1.grid(row=1,column=2,columnspan=3) 
 o1.config(fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '10'), bd=3, relief='raised',justify='center',width=20)
 
-text = players[mode.get()][turn]
-l3 = Label(root, text=text, fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'),justify='center')
-l3.grid(row=1, column=6,columnspan=4)
+chance = StringVar()
+chance_list = [players[mode.get()][0],players[mode.get()][1]] 
+chance.set('First Turn')
+l3 = OptionMenu(root,chance,*chance_list,command=who_goes_first)
+l3.grid(row=1,column=6,columnspan=4) 
+l3.config(fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'), bd=3, relief='raised',justify='center',width=20)
+
 
 b1 = Button(root,text = 'NEW GAME',fg=FONT_COLOR,bg = BACKGROUND_COLOR,font= ('Times New Roman', '15'),command=new_game,width=15,relief='raised',justify='center')
 b1.grid(row=1,column=11,columnspan=3) 
 b1.bind('<Enter>',animation_of_button)
 b1.bind('<Leave>',animation_of_button)
 
-
+## BOARD WIDGET ARE PRINTED BELOW
 for i in range(2,18):
     if i in [2,7,12,17]:
         Label(root, text='-'*220, fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '8'), bd=3, relief='flat',justify='center').grid(row=i, column=0, columnspan=15,sticky=W+E)
@@ -223,17 +239,3 @@ for i in range(2,18):
         Label(root, text='|', fg=FONT_COLOR, bg=BACKGROUND_COLOR ,font=('Times', '15'), bd=3, relief='flat',justify='center').grid(row=i, column=9)
     
 mainloop()
-'''    elif mode.get() == "Computer Impossible":
-        turn = not turn
-        if num_of_turn == 1:
-            for i in [boxes[0][0],boxes[0][2],boxes[2][0],boxes[2][2]]:
-                if i.get() == 'X':
-                    box = boxes[1][1]
-                    box.insert(0,"O")
-                    box.config(state='disabled')
-        else:
-            ans = minimax([j.get() for i in boxes for j in i],False)
-            if ans['position'] is not None:
-                box = boxes[ans['position']//3][ans['position']%3]
-                box.insert(0,"O")
-                box.config(state='disabled')'''

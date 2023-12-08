ba6b189039e59aed9f0e3a32097a46b428bb9b68
Vade-20 <vedantsaxena2012@gmail.com>
2023-12-08 21:37:42 +0530

    Finral_stared_drawing

--- Etchingdrawer/etchingdrawer.py
+++ Etchingdrawer/etchingdrawer.py
@@ -5,6 +5,7 @@ from pprint import pprint
 
 x_screen,y_screen = get_terminal_size()
 screen = [['' for i in range(x_screen)] for j in range(y_screen)]
+
 UP_DOWN_CHAR         = chr(9474)  # Character 9474 is '│'
 LEFT_RIGHT_CHAR      = chr(9472)  # Character 9472 is '─'
 DOWN_RIGHT_CHAR      = chr(9484)  # Character 9484 is '┌'
@@ -18,7 +19,7 @@ UP_LEFT_RIGHT_CHAR   = chr(9524)  # Character 9524 is '┴'
 CROSS_CHAR           = chr(9532)  # Character 9532 is '┼'
 
 def get_direction(x,y):
-    if y-1 < 0 : 
+    if y-1 > 0 : 
         if screen[y-1][x] == '':
             up = False
         else:
@@ -26,7 +27,7 @@ def get_direction(x,y):
     else: 
         up = False
     
-    if y+1 > y_screen :
+    if y+1 < y_screen :
         if screen[y+1][x] == '':
             down = False
         else:
@@ -34,7 +35,7 @@ def get_direction(x,y):
     else: 
         down = False
     
-    if x-1 < 0 : 
+    if x-1 > 0 : 
         if screen[y][x-1] == '':
             left = False
         else:
@@ -42,7 +43,7 @@ def get_direction(x,y):
     else: 
         left = False
     
-    if x+1 > x_screen :
+    if x+1 < x_screen :
         if screen[y][x+1] == '':
             right = True
         else:
@@ -52,28 +53,35 @@ def get_direction(x,y):
     
     return [up,down,left,right]
     
-def get_character(x,y):
+def get_character(x,y,char):
     direction = get_direction(x,y)
     up,down,left,right = direction
-    
+
     #For 3 Directional 
     if False not in direction:
         return CROSS_CHAR
     elif False not in direction[:2]:
         return UP_DOWN_LEFT_CHAR
-    elif False not in direction[:1]+direction[3]:
+    elif False not in direction[:1]+[direction[3]]:
         return UP_DOWN_RIGHT_CHAR
     elif False not in direction[1:]:
         return DOWN_LEFT_RIGHT_CHAR
-    elif False not in direction[0] + direction[2:]:
+    elif False not in [direction[0]] + direction[2:]:
         return UP_LEFT_RIGHT_CHAR
+    elif False not in [up, right]:
+        return UP_RIGHT_CHAR
+    elif False not in [up, left]:
+        return UP_LEFT_CHAR
+    elif False not in [down, right]:
+        return DOWN_RIGHT_CHAR
+    elif False not in [down ,left]:
+        return DOWN_RIGHT_CHAR
+    else:
+        if char in ['w','s']:
+            return UP_DOWN_CHAR
+        elif char in ['a','d']:
+            return LEFT_RIGHT_CHAR
     
-    if False not in [up,down]:
-        return UP_DOWN_CHAR
-    
-        
-    
- 
 @wrapper
 def main(stdscr):
     stdscr.clear()
@@ -81,29 +89,45 @@ def main(stdscr):
     curses.curs_set(0)
     
     x,y = 0,0
-    stdscr.addstr(y,x,'-')
+    stdscr.addstr(y,x,LEFT_RIGHT_CHAR)
     while True:
         try:    
             ch = stdscr.getkey().lower()
             if ch == 'w':
-                x -= 1
-                if screen[y][x] !=  '':
-                    get_character(x,y)
-                else:
-                    screen[y][x] = LEFT_RIGHT_CHAR
-                    stdscr.addstr(y,x,LEFT_RIGHT_CHAR)
-            elif ch == 'KEY_RIGHT':
-                screen[y][x] = LEFT_RIGHT_CHAR
-                x += 1
-                stdscr.addstr(y,x,LEFT_RIGHT_CHAR)
-            elif ch == 'KEY_UP':
-                screen[y][x] = '|'
                 y -= 1
-                stdscr.addstr(y,x,'|')
-            elif ch == 'KEY_DOWN':
-                screen[y][x] = '|'
+                if y < 0:
+                    y += 1
+                    continue
+                text = get_character(x,y,ch)
+                screen[y][x] = text
+                stdscr.addstr(y,x,text)
+            
+            if ch == 's':
                 y += 1
-                stdscr.addstr(y,x,'|')
+                if y > y_screen-1:
+                    y -= 1
+                    continue
+                text = get_character(x,y,ch)
+                screen[y][x] = text
+                stdscr.addstr(y,x,text)
+                             
+            if ch == 'a':
+                x -= 1
+                if x < 0:
+                    x += 1
+                    continue
+                text = get_character(x,y,ch)
+                screen[y][x] = text
+                stdscr.addstr(y,x,text)
+                    
+            if ch == 'd':
+                x += 1
+                if x > x_screen-1:
+                    x -= 1
+                    continue
+                text = get_character(x,y,ch)
+                screen[y][x] = text
+                stdscr.addstr(y,x,text)
+                
         except KeyboardInterrupt:
-            print(screen)
             break

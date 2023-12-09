9655a6e7fce5ef699152b8ce634979712e22d0e0
Vade-20 <vedantsaxena2012@gmail.com>
2023-12-09 10:16:35 +0530

    Failed_test

--- Etchingdrawer/etchingdrawer.py
+++ Etchingdrawer/etchingdrawer.py
@@ -4,14 +4,16 @@ from shutil import get_terminal_size
 from pprint import pprint
 
 x_screen,y_screen = get_terminal_size()
-screen = [['' for i in range(x_screen)] for j in range(y_screen)]
+screen = [['' for i in range(-2,x_screen)] for j in range(-2,y_screen)]
 
 UP_DOWN_CHAR         = chr(9474)  # Character 9474 is '│'
 LEFT_RIGHT_CHAR      = chr(9472)  # Character 9472 is '─'
 DOWN_RIGHT_CHAR      = chr(9484)  # Character 9484 is '┌'
 DOWN_LEFT_CHAR       = chr(9488)  # Character 9488 is '┐'
+
 UP_RIGHT_CHAR        = chr(9492)  # Character 9492 is '└'
 UP_LEFT_CHAR         = chr(9496)  # Character 9496 is '┘'
+
 UP_DOWN_RIGHT_CHAR   = chr(9500)  # Character 9500 is '├'
 UP_DOWN_LEFT_CHAR    = chr(9508)  # Character 9508 is '┤'
 DOWN_LEFT_RIGHT_CHAR = chr(9516)  # Character 9516 is '┬'
@@ -45,7 +47,7 @@ def get_direction(x,y):
     
     if x+1 < x_screen :
         if screen[y][x+1] == '':
-            right = True
+            right = False
         else:
             right = True
     else:
@@ -56,31 +58,16 @@ def get_direction(x,y):
 def get_character(x,y,char):
     direction = get_direction(x,y)
     up,down,left,right = direction
-
-    #For 3 Directional 
-    if False not in direction:
-        return CROSS_CHAR
-    elif False not in direction[:2]:
-        return UP_DOWN_LEFT_CHAR
-    elif False not in direction[:1]+[direction[3]]:
-        return UP_DOWN_RIGHT_CHAR
-    elif False not in direction[1:]:
-        return DOWN_LEFT_RIGHT_CHAR
-    elif False not in [direction[0]] + direction[2:]:
-        return UP_LEFT_RIGHT_CHAR
-    elif False not in [up, right]:
-        return UP_RIGHT_CHAR
-    elif False not in [up, left]:
-        return UP_LEFT_CHAR
-    elif False not in [down, right]:
-        return DOWN_RIGHT_CHAR
-    elif False not in [down ,left]:
-        return DOWN_RIGHT_CHAR
-    else:
-        if char in ['w','s']:
-            return UP_DOWN_CHAR
-        elif char in ['a','d']:
-            return LEFT_RIGHT_CHAR
+    text = screen[y][x]
+    if char == 's':
+        prev_text = screen[y][x-1]
+        prev_text_up = screen[y-1][x-1]
+        prev_down_up = screen[y+1][x-1]
+        if prev_text == '|' and prev_down_up != '':
+            pass
+            
+        
+    
     
 @wrapper
 def main(stdscr):
